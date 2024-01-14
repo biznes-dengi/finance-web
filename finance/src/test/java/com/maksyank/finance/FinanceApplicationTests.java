31415f175364148642772e1d36ed83d5ac7354bd
Mikita Yankouski <nikitayankouski@gmail.com>
2024-01-14 15:02:52 +0100

    switch to Spring

---
+++ finance/src/test/java/com/maksyank/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksyank.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
