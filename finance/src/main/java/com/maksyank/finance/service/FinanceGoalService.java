560bd6929ece7f74bea550df4a2e3b96e2ce32c8
Mikita Yankouski <nikitayankouski@gmail.com>
2024-01-15 23:31:03 +0100

    add templates of services: FinanceGoal, Deposit, FundHistory

---
+++ finance/src/main/java/com/maksyank/finance/service/FinanceGoalService.java
@@ -0,0 +1,26 @@
+package com.maksyank.finance.service;
+
+import com.maksyank.finance.boundary.financegoal.request.PreparedFinanceGoal;
+import org.springframework.stereotype.Service;
+
+// TO DO add validation to all methods
+@Service
+public class FinanceGoalService {
+
+    FinanceGoalService() {}
+
+    public void findById(int id) {
+    }
+
+    public void findByStatus (String status) {
+    }
+
+    public void create(PreparedFinanceGoal preparedFinanceGoal) {
+    }
+
+    public void update(int id, PreparedFinanceGoal preparedFinanceGoal) {
+    }
+
+    public void delete(int id) {
+    }
+}
