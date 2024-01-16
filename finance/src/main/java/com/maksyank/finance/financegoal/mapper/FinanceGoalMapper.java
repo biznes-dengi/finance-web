93360e000c12627444d442c00cb867b931903916
Mikita Yankouski <nikitayankouski@gmail.com>
2024-01-16 19:17:42 +0100

    add mapper config

---
+++ finance/src/main/java/com/maksyank/finance/financegoal/mapper/FinanceGoalMapper.java
@@ -0,0 +1,31 @@
+package com.maksyank.finance.financegoal.mapper;
+
+import com.maksyank.finance.financegoal.domain.common.FinanceGoal;
+import com.maksyank.finance.financegoal.domain.dto.FinanceGoalDTO;
+import com.maksyank.finance.financegoal.domain.response.FinanceGoalResponse;
+import com.maksyank.finance.financegoal.domain.response.FinanceGoalViewResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface FinanceGoalMapper {
+
+    // TO DO remove that ignoring
+    @Mapping(target = "userAccount", ignore = true)
+    @Mapping(target = "deposits", ignore = true)
+    FinanceGoalDTO sourceToDto(FinanceGoal source);
+
+    FinanceGoal dtoToSource(FinanceGoalDTO dto);
+
+    @Mapping(target = "state", ignore = true)
+    @Mapping(target = "description", ignore = true)
+    @Mapping(target = "deadline", ignore = true)
+    @Mapping(target = "riskProfile", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "lastChange", ignore = true)
+    FinanceGoalResponse dtoToResponse(FinanceGoalDTO dto);
+
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "lastChange", ignore = true)
+    FinanceGoalViewResponse dtoToViewResponse(FinanceGoalDTO dto);
+}
