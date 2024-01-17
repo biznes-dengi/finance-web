2afccd708d4259fc706ba5eedf96c4c0f2fb2abd
Mikita Yankouski <nikitayankouski@gmail.com>
2024-01-17 12:12:38 +0100

    MapStruct to do later

--- finance/src/main/java/com/maksyank/finance/financegoal/mapper/FinanceGoalMapperToDoLater.java
+++ finance/src/main/java/com/maksyank/finance/financegoal/mapper/FinanceGoalMapperToDoLater.java
@@ -8,14 +8,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface FinanceGoalMapper {
+public interface FinanceGoalMapperToDoLater {
 
     // TO DO remove that ignoring
     @Mapping(target = "userAccount", ignore = true)
     @Mapping(target = "deposits", ignore = true)
     FinanceGoalDTO sourceToDto(FinanceGoal source);
-
-    FinanceGoal dtoToSource(FinanceGoalDTO dto);
 
     @Mapping(target = "state", ignore = true)
     @Mapping(target = "description", ignore = true)
@@ -23,8 +21,18 @@ public interface FinanceGoalMapper {
     @Mapping(target = "riskProfile", ignore = true)
     @Mapping(target = "createdOn", ignore = true)
     @Mapping(target = "lastChange", ignore = true)
+    FinanceGoalViewResponse sourceToViewResponse(FinanceGoal source);
+
+    FinanceGoal dtoToSource(FinanceGoalDTO dto);
+
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "lastChange", ignore = true)
     FinanceGoalResponse dtoToResponse(FinanceGoalDTO dto);
 
+    @Mapping(target = "state", ignore = true)
+    @Mapping(target = "description", ignore = true)
+    @Mapping(target = "deadline", ignore = true)
+    @Mapping(target = "riskProfile", ignore = true)
     @Mapping(target = "createdOn", ignore = true)
     @Mapping(target = "lastChange", ignore = true)
     FinanceGoalViewResponse dtoToViewResponse(FinanceGoalDTO dto);
