1f7f71aa91d697b9ba933b51340a5c14b0bf586b
Mikita Yankouski <nikitayankouski@gmail.com>
2024-02-10 14:57:29 +0100

    add GET amount byMonth to deposit end-point

--- finance/src/main/java/com/maksyank/finance/financegoal/repository/DepositRepository.java
+++ finance/src/main/java/com/maksyank/finance/financegoal/repository/DepositRepository.java
@@ -5,12 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Optional;
-
 public interface DepositRepository extends JpaRepository<Deposit, Integer>{
     Slice<Deposit> findAllByFinanceGoal_Id(int financeGoalId, Pageable pageable);
-
-    Optional<Collection<Deposit>> findDepositsByFundingDateLessThanEqual(LocalDateTime date);
 }
---
+++ finance/src/main/java/com/maksyank/finance/financegoal/service/process/FundProcess.java
@@ -0,0 +1,52 @@
+package com.maksyank.finance.financegoal.service.process;
+
+import com.maksyank.finance.financegoal.domain.common.Deposit;
+import com.maksyank.finance.financegoal.domain.common.FinanceGoal;
+import com.maksyank.finance.financegoal.exception.NotFoundException;
+import com.maksyank.finance.financegoal.service.repoimpl.FinanceGoalRepoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class FundProcess {
+    private FinanceGoalRepoImpl financeGoalRepoImpl;
+
+    @Autowired
+    FundProcess(FinanceGoalRepoImpl financeGoalRepoImpl) {
+        this.financeGoalRepoImpl = financeGoalRepoImpl;
+    }
+
+    public BigDecimal processGetFundAmountByMonth(int financeGoalId, int year, int month, int userId) throws NotFoundException {
+        final var financeGoal = this.financeGoalRepoImpl.findByIdAndUserId(financeGoalId, userId);
+
+        final var startMonth = LocalDateTime.of(year, month, YearMonth.of(year, month).lengthOfMonth(), 23, 59, 59);
+        final var endMonth = LocalDateTime.of(year, month, 1, 0, 0, 0);
+
+        final var foundDepositsByMonth = this.findFundDepositsByMonth(financeGoal, startMonth, endMonth);
+        if (foundDepositsByMonth.size() == 0) {
+            throw new NotFoundException("Entities 'Deposit' were not found in " + year + "/" + month);
+        }
+        return this.computeSumAmount(foundDepositsByMonth);
+    }
+
+    // TODO critical point. For big data troubles with time of response
+    // TODO change filter fund to enum
+    // TODO maybe split logic into methods by filters. It relates from if there's a need for it
+    private List<Deposit> findFundDepositsByMonth(FinanceGoal source, LocalDateTime beginMonth, LocalDateTime endMonth) {
+        return source.getDeposits().stream()
+                .filter(finGoal -> finGoal.getFundingDate().isAfter(beginMonth) && finGoal.getFundingDate().isBefore(endMonth))
+                .filter(figGoal -> Objects.equals(figGoal.getType(), "fund"))
+                .toList();
+    }
+
+    // TODO maybe combine with findFundDepositsByMonth() for best performance
+    private BigDecimal computeSumAmount(List<Deposit> source) {
+        return source.stream().map(Deposit::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
