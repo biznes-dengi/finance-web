c41c19135560f0afc4704b277d474dd4fcd137fb
Mikita Yankouski <nikitayankouski@gmail.com>
2024-02-27 17:48:43 +0100

    impl enum in Deposit domain

--- finance/src/main/java/com/maksyank/finance/financegoal/boundary/request/DepositSaveRequest.java
+++ finance/src/main/java/com/maksyank/finance/financegoal/boundary/request/DepositSaveRequest.java
@@ -1,10 +1,12 @@
 package com.maksyank.finance.financegoal.boundary.request;
 
+import com.maksyank.finance.financegoal.domain.enums.TransactionType;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record DepositSaveRequest(
-        String type,
+        TransactionType type,
         String description,
         LocalDateTime fundingDate,
         BigDecimal amount
--- finance/src/main/java/com/maksyank/finance/financegoal/boundary/response/DepositResponse.java
+++ finance/src/main/java/com/maksyank/finance/financegoal/boundary/response/DepositResponse.java
@@ -1,11 +1,13 @@
 package com.maksyank.finance.financegoal.boundary.response;
 
+import com.maksyank.finance.financegoal.domain.enums.TransactionType;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record DepositResponse(
         int id,
-        String type,
+        TransactionType type,
         String description,
         LocalDateTime fundingDate,
         BigDecimal amount
