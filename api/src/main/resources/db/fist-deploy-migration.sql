956b8c6ca6b63136795adc6f4ecef13fd1f877d8
Mikita Yankouski <nikitayankouski@gmail.com>
2024-03-13 16:12:24 +0100

    rename root directory

--- api/src/main/resources/db/fist-deploy-migration.sql
+++ api/src/main/resources/db/fist-deploy-migration.sql
@@ -6,6 +6,7 @@ CREATE TYPE app_role AS ENUM ('admin', 'user');
 CREATE TYPE user_gender AS ENUM ('male', 'female');
 CREATE TYPE risk_profile_type AS ENUM ('conservative', 'moderate', 'aggressive');
 CREATE TYPE transaction_type AS ENUM ('FUND', 'WITHDRAW');
+CREATE TYPE fin_goal_image_type AS ENUM ('JPEG', 'JPG', 'PNG');
 
 CREATE TABLE user_account (
     id_user_account SERIAL PRIMARY KEY,
@@ -32,11 +33,12 @@ CREATE TABLE finance_goal (
     deadline TIMESTAMP,
     risk_profile risk_profile_type,
     image bytea,
-    type_image VARCHAR(4),
+    type_image fin_goal_image_type,
     created_on TIMESTAMP NOT NULL,
     last_change TIMESTAMP,
     id_user_account INT NOT NULL,
-    FOREIGN KEY (id_user_account) REFERENCES user_account(id_user_account)
+    FOREIGN KEY (id_user_account) REFERENCES user_account(id_user_account),
+    CHECK (last_change > created_on)
 );
 
 CREATE TABLE deposit (
@@ -46,5 +48,5 @@ CREATE TABLE deposit (
     funding_date TIMESTAMP NOT NULL,
     amount NUMERIC(38,2) NOT NULL,
     id_finance_goal INT NOT NULL,
-    FOREIGN KEY (id_finance_goal) REFERENCES finance_goal(id_goal)
+    FOREIGN KEY (id_finance_goal) REFERENCES finance_goal(id_goal),
 );
