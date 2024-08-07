cf03c8818cae4c9f6bca4c13af9c5c92f99d5eed
Mikita Yankouski <nikitayankouski@gmail.com>
2024-08-07 09:06:15 +0200

    add TransactionMapper

--- api/src/main/java/com/maksyank/finance/saving/mapper/TransactionMapper.java
+++ api/src/main/java/com/maksyank/finance/saving/mapper/TransactionMapper.java
@@ -1,40 +1,31 @@
 package com.maksyank.finance.saving.mapper;
 
-import com.maksyank.finance.saving.boundary.request.TransactionUpdateRequest;
-import com.maksyank.finance.saving.domain.Transaction;
-import com.maksyank.finance.saving.domain.Saving;
 import com.maksyank.finance.saving.boundary.request.TransactionRequest;
+import com.maksyank.finance.saving.boundary.request.TransactionUpdateRequest;
 import com.maksyank.finance.saving.boundary.response.TransactionResponse;
 import com.maksyank.finance.saving.boundary.response.TransactionViewResponse;
+import com.maksyank.finance.saving.domain.Transaction;
 import com.maksyank.finance.saving.dto.TransactionDto;
 import com.maksyank.finance.saving.dto.TransactionUpdateDto;
+import org.mapstruct.*;
 
-import java.util.Collection;
 import java.util.List;
 
-public class TransactionMapper {
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface TransactionMapper {
+    TransactionResponse transactionToTransactionResponse(Transaction source);
 
-    public static TransactionResponse entityToResponse(Transaction source) {
-        return new TransactionResponse(source.getId(), source.getType(), source.getDescription(), source.getDeal_date(), source.getAmount());
-    }
+    TransactionViewResponse transactionToTransactionViewResponse(Transaction source);
 
-    public static TransactionViewResponse entityToViewResponse(Transaction source) {
-        return new TransactionViewResponse(source.getId(), source.getType(), source.getDeal_date(), source.getAmount());
-    }
+    List<TransactionViewResponse> transactionListToTransactionViewResponseList(List<Transaction> source);
 
-    public static List<TransactionViewResponse> entityToViewResponse(Collection<Transaction> source) {
-        return source.stream().map(TransactionMapper::entityToViewResponse).toList();
-    }
+    TransactionDto transactionRequestToTransactionDto(TransactionRequest source);
 
-    public static Transaction mapToNewEntity(TransactionDto source, Saving saving) {
-        return new Transaction(source.type(), source.description(), source.fundingDate(), source.amount(), saving);
-    }
-
-    public static TransactionDto mapToDto(TransactionRequest source) {
-        return new TransactionDto(source.type(), source.description(), source.fundingDate(), source.amount());
-    }
-
-    public static TransactionUpdateDto mapToUpdateDto(TransactionUpdateRequest source) {
-        return new TransactionUpdateDto(source.description());
-    }
+    TransactionUpdateDto transactionUpdateRequestToTransactionUpdateDto(TransactionUpdateRequest source);
 }
