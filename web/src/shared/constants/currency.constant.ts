28d654aea224acdc16288a8ca9c29302b6b31533
antonmaksimow <anton.maksimau@leverx-group.com>
2024-09-09 17:40:37 +0300

    update currency.constant.ts

--- web/src/shared/constants/currency.constant.ts
+++ web/src/shared/constants/currency.constant.ts
@@ -1,7 +1,30 @@
 export enum CURRENCY {
-	USD,
-	PLN,
-	BYN,
-	EUR,
-	GBP,
+	USD = 'usd',
+	PLN = 'pln',
+	BYN = 'byn',
+	EUR = 'eur',
+	GBP = 'gbp',
 }
+
+export const CURRENCY_MAP = {
+	[CURRENCY.USD]: {
+		code: 'USD',
+		symbol: '$',
+	},
+	[CURRENCY.PLN]: {
+		code: 'USD',
+		symbol: '$',
+	},
+	[CURRENCY.BYN]: {
+		code: 'USD',
+		symbol: '$',
+	},
+	[CURRENCY.EUR]: {
+		code: 'USD',
+		symbol: '$',
+	},
+	[CURRENCY.GBP]: {
+		code: 'USD',
+		symbol: '$',
+	},
+};
