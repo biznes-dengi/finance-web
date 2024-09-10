e49330eb4916e8e16e059419490b2af8ccdfb473
antonmaksimow <anton.maksimau@leverx-group.com>
2024-09-10 15:13:26 +0300

    update close-dialog icon styles, add duration-300 to drawer

--- web/src/shared/ui/Dialog.tsx
+++ web/src/shared/ui/Dialog.tsx
@@ -14,13 +14,19 @@ export function Dialog(props: Props) {
 	const {children, isDialogOpen, onClose} = props;
 
 	return (
-		<Drawer anchor='bottom' open={isDialogOpen} onClose={onClose} PaperProps={{className: 'rounded-t-2xl'}}>
+		<Drawer
+			anchor='bottom'
+			open={isDialogOpen}
+			onClose={onClose}
+			PaperProps={{className: 'rounded-t-2xl'}}
+			transitionDuration={300}
+		>
 			<Box className='flex flex-col px-6 py-4'>
 				<Box className='flex items-center justify-between' baseMarginBottom>
 					<div />
 					<Box className='text-xl font-medium'>Savings</Box>
 					<div
-						className='flex h-6 w-6 items-center justify-center duration-300 hover:cursor-pointer hover:rounded-full hover:bg-secondary-grey'
+						className='flex h-7 w-7 items-center justify-center duration-300 hover:cursor-pointer hover:rounded-full hover:bg-secondary-grey'
 						onClick={onClose}
 					>
 						<div className='h-5 w-5'>{Icon.x}</div>
