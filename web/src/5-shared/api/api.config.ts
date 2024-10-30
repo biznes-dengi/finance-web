fb1468e6cf4d08e436a793db76826fb5bf77dac7
antonmaksimow <anton.maksimau@leverx-group.com>
2024-10-30 13:02:01 +0300

    update axiosInstance headers

--- web/src/5-shared/api/api.config.ts
+++ web/src/5-shared/api/api.config.ts
@@ -4,8 +4,8 @@ export const axiosInstance = axios.create({
 	baseURL: 'http://localhost:8080',
 	headers: {
 		'Content-Type': 'application/json',
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Request-Private-Network': true,
+		// 'Access-Control-Allow-Origin': '*',
+		// 'Access-Control-Request-Private-Network': true,
 		withCredentials: true,
 	},
 });
