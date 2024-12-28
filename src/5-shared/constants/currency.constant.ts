32b78e44b66ffd5c0fdf45d6015823f756928017
tonycashbbb <tonycashbbb@gmail.com>
2024-12-28 18:41:06 +0300

    add only USD support

--- src/5-shared/constants/currency.constant.ts
+++ src/5-shared/constants/currency.constant.ts
@@ -1,27 +1,27 @@
 export enum CURRENCY {
 	USD = 'USD',
-	EUR = 'EUR',
-	BYN = 'BYN',
-	RUB = 'RUB',
+	// EUR = 'EUR',
+	// BYN = 'BYN',
+	// RUB = 'RUB',
 }
 
 export const CURRENCY_SYMBOL = {
 	[CURRENCY.USD]: '$',
-	[CURRENCY.EUR]: '€',
-	[CURRENCY.BYN]: 'Br',
-	[CURRENCY.RUB]: '₽',
+	// [CURRENCY.EUR]: '€',
+	// [CURRENCY.BYN]: 'Br',
+	// [CURRENCY.RUB]: '₽',
 };
 
 export const CURRENCY_CODE = {
 	[CURRENCY.USD]: 'USD',
-	[CURRENCY.EUR]: 'EUR',
-	[CURRENCY.BYN]: 'BYN',
-	[CURRENCY.RUB]: 'RUB',
+	// [CURRENCY.EUR]: 'EUR',
+	// [CURRENCY.BYN]: 'BYN',
+	// [CURRENCY.RUB]: 'RUB',
 };
 
 export const CURRENCY_OPTIONS = [
 	{description: 'USD', name: 'US Dollar', value: CURRENCY.USD},
-	{description: 'EUR', name: 'Euro', value: CURRENCY.EUR},
-	{description: 'RUB', name: 'Russian ruble', value: CURRENCY.RUB},
-	{description: 'BYN', name: 'Belarusian ruble', value: CURRENCY.BYN},
+	// {description: 'EUR', name: 'Euro', value: CURRENCY.EUR},
+	// {description: 'RUB', name: 'Russian ruble', value: CURRENCY.RUB},
+	// {description: 'BYN', name: 'Belarusian ruble', value: CURRENCY.BYN},
 ];
