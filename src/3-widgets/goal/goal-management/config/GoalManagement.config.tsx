2fb8aa8e1eb74721d4aad676ecda1939244d5541
toxa <tonycashbbb@gmail.com>
2025-01-17 22:44:56 +0300

    update button name to check jobs in ci/cd

--- src/3-widgets/goal/goal-management/config/GoalManagement.config.tsx
+++ src/3-widgets/goal/goal-management/config/GoalManagement.config.tsx
@@ -9,7 +9,7 @@ export const buttonConfigs = [
 		onClick: ({navigate}) => navigate(APP_PATH.createGoal),
 	},
 	{
-		name: 'Add money 2',
+		name: 'Add money',
 		type: ButtonType.icon,
 		icon: <Icon type='fund' />,
 		onClick: ({navigate}) => navigate(APP_PATH.goalFund),
