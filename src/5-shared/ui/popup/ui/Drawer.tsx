11a407e0a0c36c69e25ef1c470defdce4dd7e86a
toxa <tonycashbbb@gmail.com>
2025-02-14 17:35:18 +0300

    fix drawer box-shadow displaying

--- src/5-shared/ui/popup/ui/Drawer.tsx
+++ src/5-shared/ui/popup/ui/Drawer.tsx
@@ -26,11 +26,11 @@ export function Drawer(props: DrawerProps) {
 				<Overlay className='fixed inset-0 bg-black/40' />
 
 				<Content className='fixed bottom-0 left-0 right-0 max-h-[94vh] rounded-t-[28px] bg-light-grey outline-none transition-all duration-200'>
-					<div className='mx-auto flex h-full w-full max-w-md flex-col p-4 pt-2'>
+					<div className='mx-auto flex h-full w-full max-w-md flex-col p-2'>
 						<div className='mx-auto mb-3 h-[3px] w-9 rounded-full bg-[#BAC3CA]' />
 						<Title className={cn(title ? 'mb-3 text-center font-medium' : 'hidden')}>{title}</Title>
-						{/* Контейнер для прокручиваемого содержимого */}
-						<div className='custom-scrollbar max-h-[84vh] flex-1 overflow-y-auto'>{children}</div>
+						{/* Контейнер для cкролла */}
+						<div className='custom-scrollbar max-h-[84vh] flex-1 overflow-y-auto p-2 pt-0'>{children}</div>
 					</div>
 				</Content>
 			</Portal>
