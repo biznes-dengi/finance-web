bfd863eab5e8de9fced33d4157d83b8258c6fab3
toxa <tonycashbbb@gmail.com>
2025-02-14 16:41:07 +0300

    update Drawer styles

--- src/5-shared/ui/popup/ui/Drawer.tsx
+++ src/5-shared/ui/popup/ui/Drawer.tsx
@@ -25,10 +25,10 @@ export function Drawer(props: DrawerProps) {
 			<Portal>
 				<Overlay className='fixed inset-0 bg-black/40' />
 
-				<Content className='fixed bottom-0 left-0 right-0 max-h-[94vh] rounded-t-3xl bg-light-grey outline-none transition-all duration-200'>
+				<Content className='fixed bottom-0 left-0 right-0 max-h-[94vh] rounded-t-[28px] bg-light-grey outline-none transition-all duration-200'>
 					<div className='mx-auto flex h-full w-full max-w-md flex-col p-4 pt-2'>
-						<div className='mx-auto mb-4 h-1 w-12 rounded-full bg-[#BAC3CA]' />
-						<Title className={cn(title ? 'mb-4 text-center text-xl font-medium' : 'hidden')}>{title}</Title>
+						<div className='mx-auto mb-3 h-1 w-9 rounded-full bg-[#BAC3CA]' />
+						<Title className={cn(title ? 'mb-3 text-center font-medium' : 'hidden')}>{title}</Title>
 						{/* Контейнер для прокручиваемого содержимого */}
 						<div className='custom-scrollbar max-h-[84vh] flex-1 overflow-y-auto'>{children}</div>
 					</div>
