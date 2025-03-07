d04832f192b78079918efd2c64d61ee61d784374
toxa <tonycashbbb@gmail.com>
2025-03-07 12:25:06 +0300

    refactor project structure

--- src/2-pages/portfolio/ui/PortfolioWalletListPage.tsx
+++ src/2-pages/portfolio/ui/PortfolioWalletListPage.tsx
@@ -1,14 +1,32 @@
 import {PageWidgetsWrapper} from '@pages/ui';
-import {PortfolioWalletList} from '@widgets/portfolio';
-import {PageHeader} from '@shared/ui';
+import {Icon, Item, List, PageHeader, TextField} from '@shared/ui';
 import {APP_PATH, APP_TEXT} from '@shared/constants';
+import {useNavigate} from 'react-router-dom';
+
+const wallets = [
+	{name: 'Phantom мемасы', description: '0x812731...12L1bb2sk'},
+	{name: 'Metamask долгосрок ETH', description: '0x8193921...2348H6lsk'},
+	{name: 'Краткосрок флиппинг', description: '0x7613921...2Lq8H6lsk'},
+];
 
 export function PortfolioWalletListPage() {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<PageHeader title={APP_TEXT.connectedWallets} backPath={APP_PATH.portfolio.list} />
 			<PageWidgetsWrapper>
-				<PortfolioWalletList />
+				<TextField value={''} onChange={() => {}} placeholder={APP_TEXT.search} isSearch />
+				<List
+					items={wallets}
+					renderItem={(wallet) => (
+						<Item
+							{...wallet}
+							image={<Icon type='wallet' withBackground />}
+							onClick={() => navigate(APP_PATH.portfolio.getItemWalletDetailsPath('1'))}
+						/>
+					)}
+				/>
 			</PageWidgetsWrapper>
 		</>
 	);
